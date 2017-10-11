15834efc2edbb772bbb0596e9d48f78cedda3b54
chipable <pager6@students.rowan.edu>
2017-10-11 00:41:59 -0400

    added comments

--- Milestone_StrangerThings/main.c
+++ Milestone_StrangerThings/main.c
@@ -64,29 +64,29 @@ int main(void)
 //initialize timer B0
 	TB0CTL = ( MC__UP  + TBSSEL__SMCLK + ID__8);    //Set to up mode, run at SMCLK DIVIDE BY 8
 	TB0EX0 = TBIDEX_3;      // DIVIDE BY 4
-	TB0CCTL0 = CCIE;
+	TB0CCTL0 = CCIE;    // enable interrupts
 	TB0CCR0 = 255;      //set PWM frequency to 1 KHz
 	TB0CCR1 = 0;        //initialize red Duty cycle to CCR1
-	TB0CCTL1 = CCIE;
+	TB0CCTL1 = CCIE;    // enable interrupts
 	TB0CCR2 = 0;        //initialize green PWM to CCR2
-	TB0CCTL2 = CCIE;
+	TB0CCTL2 = CCIE;    // enable interrupts
 
 //initialize timer B1
 	TB1CTL = ( MC__UP  + TBSSEL__SMCLK + ID__8);    //Set to up mode, run at SMCLK0 DIVIDE BY 8
     TB1EX0 = TBIDEX_3;      // DIVIDE BY 4
-	TB1CCTL0 = CCIE;
+	TB1CCTL0 = CCIE;    // enable interrupts
 	TB1CCR0 = 255;      //set PWM frequency to 2 Khz
 	TB1CCR1 = 0;        //initialize blue PWM to B1CCR1
-	TB1CCTL1 = CCIE;
+	TB1CCTL1 = CCIE;    // enable interrupts
 
 	/* UART INIT */
-	  __bis_SR_register(SCG0);                 // disable FLL
+	  __bis_SR_register(SCG0);                 // disable freqrency level lock
 	  CSCTL3 |= SELREF__REFOCLK;               // Set REFO as FLL reference source
 	  CSCTL1 = DCOFTRIMEN_1 | DCOFTRIM0 | DCOFTRIM1 | DCORSEL_3;// DCOFTRIM=3, DCO Range = 8MHz
 	  CSCTL2 = FLLD_0 + 243;                  // DCODIV = 8MHz
 	  __delay_cycles(3);
-	  __bic_SR_register(SCG0);                // enable FLL
-	  Software_Trim();                        // Software Trim to get the best DCOFTRIM value
+	  __bic_SR_register(SCG0);                // enable freqrency level lock
+	  Software_Trim();                        // TI's code to get accurate uart clock
 
 	  CSCTL4 = SELMS__DCOCLKDIV | SELA__REFOCLK; // set default REFO(~32768Hz) as ACLK source, ACLK = 32768Hz
 	                                           // default DCODIV as MCLK and SMCLK source
@@ -96,7 +96,7 @@ int main(void)
 
 	  // Configure UART
 	  UCA0CTLW0 |= UCSWRST;
-	  UCA0CTLW0 |= UCSSEL__SMCLK;
+	  UCA0CTLW0 |= UCSSEL__SMCLK;           // use smclk
 
 	  // Baud Rate calculation
 	  // 8000000/(16*9600) = 52.083
@@ -115,10 +115,10 @@ int main(void)
 /*ISR FOR TIMERB0 CCR0*/
 #pragma vector=TIMER0_B0_VECTOR
 __interrupt void Timer0_B0_ISR (void){
-    if(TB0CCR1 != 0)
+    if(TB0CCR1 != 0)          //if duty cycle is 0 keep output high
         P1OUT &= ~BIT0;
 
-    if(TB0CCR2 != 0)
+    if(TB0CCR2 != 0)        //if duty cycle is 0 keep output high
         P1OUT &= ~BIT1;
 
     TB0CCTL0 &= ~BIT0;    //clears flags
@@ -129,13 +129,13 @@ __interrupt void Timer0_B1_ISR (void){
 
     switch (TB0IV){
     case 0x02:      //CCR1
-        if(TB0CCR1 != 255)
+        if(TB0CCR1 != 255)      //if duty cycle is 100%  keep output low
             P1OUT |= BIT0;
 
         TB0CCTL1 &= ~BIT0;    //clears flags
         break;
     case 0x04:      //CCR2
-        if(TB0CCR2 != 255)
+        if(TB0CCR2 != 255)      //if duty cycle is 100%  keep output low
             P1OUT |= BIT1;
 
         TB0CCTL2 &= ~BIT0;    //clears flags
@@ -146,7 +146,7 @@ __interrupt void Timer0_B1_ISR (void){
 /*ISR FOR TIMERB1 CCR0*/
 #pragma vector=TIMER1_B0_VECTOR
 __interrupt void Timer1_B0_ISR (void){
-    if(TB1CCR1 != 0)
+    if(TB1CCR1 != 0)            //if duty cycle is 0 keep output high
         P1OUT &= ~BIT2;
 
     TB1CCTL0 &= ~BIT0;    //clears flags
@@ -154,7 +154,7 @@ __interrupt void Timer1_B0_ISR (void){
 /*ISR FOR TIMERB1 CCR1*/
 #pragma vector=TIMER1_B1_VECTOR
 __interrupt void Timer1_B1_ISR (void){
-    if(TB1CCR1 != 255)
+    if(TB1CCR1 != 255)      //if duty cycle is 100%  keep output low
         P1OUT |= BIT2;
 
     TB1CCTL1 &= ~BIT0;    //clears flags
